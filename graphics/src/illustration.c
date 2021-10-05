b703be0ef3cbfb0568bfe4c7f6b659011cbc6d2a
Greg Attra <greg.attra@gmail.com>
2021-10-05 11:13:22 -0400

    finished illutration

--- graphics/src/illustration.c
+++ graphics/src/illustration.c
@@ -11,37 +11,69 @@
 #include "line.h"
 #include "point.h"
 #include "elipse.h"
+#include <math.h>
 #include <stdlib.h>
 
 #define WHEEL_RADIUS 50.0
-#define REAR_WHEEL_X 300
-#define REAR_WHEEL_Y 400
+#define REAR_WHEEL_X 150
+#define REAR_WHEEL_Y 200
 #define BASE_WIDTH 75.0
+#define IMG_HEIGHT 300
+#define IMG_WIDTH 500
 
 Color BLACK = {0.0, 0.0, 0.0};
 Color BLUE = {0.0, 0.0, 1.0};
+Color GREEN = {0.0, 1.0, 0.0};
 
-static void draw_spokes(Circle *circ, Image *src, Color col)
+static void draw_spoke(int x, int y, int r, float a, Image *src, Color col)
 {
+    Line spoke;
+    line_set2D(
+        &spoke,
+        x + r * sin(a * (M_PI / 3)),
+        y + r * cos(a * (M_PI / 3)),
+        x + r * sin(M_PI + a * (M_PI / 3)),
+        y + r * cos(M_PI + a * (M_PI / 3)));
+    line_draw(&spoke, src, col);
+}
 
+static void draw_spokes(Circle wheel, Image *src, Color col)
+{
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 1.0, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 1.5, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 2.0, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 2.5, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 3.0, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 3.5, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 4.0, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 4.5, src, BLACK);
+   draw_spoke(wheel.center.val[0], wheel.center.val[1], wheel.radius, 5.0, src, BLACK);
 }
 
 int main(int argc, char *argv[])
 {
     Image *src;
-    src = image_create(600, 900);
-    image_fillrgb(src, 1.0, 1.0, 1.0);
+    src = image_create(IMG_HEIGHT, IMG_WIDTH);
+    image_fillrgb(src, 214.0/255.0, 203.0/255.0, 171.0/255.0);
 
     Circle rear_wheel;
     Point rear_wheel_center;
     point_set2D(&rear_wheel_center, REAR_WHEEL_X, REAR_WHEEL_Y);
     circle_set(&rear_wheel, rear_wheel_center, WHEEL_RADIUS);
     circle_draw(&rear_wheel, src, BLACK);
+    circle_set(&rear_wheel, rear_wheel.center, rear_wheel.radius + 1);
+    circle_draw(&rear_wheel, src, BLACK);
+    circle_set(&rear_wheel, rear_wheel.center, rear_wheel.radius + 2);
+    circle_draw(&rear_wheel, src, BLACK);
 
     Circle front_wheel;
     Point front_wheel_center;
     point_set2D(&front_wheel_center, REAR_WHEEL_X + BASE_WIDTH + 2*WHEEL_RADIUS, REAR_WHEEL_Y);
     circle_set(&front_wheel, front_wheel_center, WHEEL_RADIUS);
+    circle_draw(&front_wheel, src, BLACK);
+    circle_set(&front_wheel, front_wheel.center, front_wheel.radius + 1);
+    circle_draw(&front_wheel, src, BLACK);
+    circle_set(&front_wheel, front_wheel.center, front_wheel.radius + 2);
     circle_draw(&front_wheel, src, BLACK);
 
     Point base_start;
@@ -117,6 +149,40 @@ int main(int argc, char *argv[])
             handlebar_brace.b.val[1] + 5.0);
     line_draw(&handlebar, src, BLACK);
 
+    Line peddal_connector;
+    line_set2D(
+        &peddal_connector,
+        base_end.val[0],
+        base_end.val[1],
+        base_end.val[0] + 10.0,
+        base_end.val[1] + 25.0);
+    line_draw(&peddal_connector, src, BLUE);
+
+    Line peddal_1;
+    line_set2D(
+        &peddal_1,
+        peddal_connector.b.val[0] - 5.0,
+        peddal_connector.b.val[1],
+        peddal_connector.b.val[0] + 5.0,
+        peddal_connector.b.val[1]);
+    Line peddal_2;
+    line_set2D(
+        &peddal_2,
+        peddal_connector.b.val[0] - 5.0,
+        peddal_connector.b.val[1] - 1,
+        peddal_connector.b.val[0] + 5.0,
+        peddal_connector.b.val[1] - 1);
+    Line peddal_3;
+    line_set2D(
+        &peddal_3,
+        peddal_connector.b.val[0] - 5.0,
+        peddal_connector.b.val[1] - 2,
+        peddal_connector.b.val[0] + 5.0,
+        peddal_connector.b.val[1] - 2);
+    line_draw(&peddal_1, src, BLACK);
+    line_draw(&peddal_2, src, BLACK);
+    line_draw(&peddal_3, src, BLACK);
+
     Elipse seat;
     elipse_set(
             &seat,
@@ -125,6 +191,53 @@ int main(int argc, char *argv[])
             5.0,
             0.0);
     elipse_drawFill(&seat, src, BLACK);
+
+    Circle front_wheel_cap;
+    circle_set(
+        &front_wheel_cap,
+        front_wheel_center,
+        7.5);
+    Circle rear_wheel_cap;
+    circle_set(
+        &rear_wheel_cap,
+        rear_wheel_center,
+        7.5);
+    circle_drawFill(&front_wheel_cap, src, BLACK);
+    circle_drawFill(&rear_wheel_cap, src, BLACK);
+
+    Circle main_gear;
+    circle_set(
+        &main_gear,
+        base_end,
+        15.0);
+    circle_draw(&main_gear, src, BLACK);
+
+    Line top_chain;
+    line_set2D(
+        &top_chain,
+        rear_wheel_cap.center.val[0],
+        rear_wheel_cap.center.val[1] + rear_wheel_cap.radius,
+        main_gear.center.val[0],
+        main_gear.center.val[1] + main_gear.radius);
+    Line bottom_chain;
+    line_set2D(
+        &bottom_chain,
+        rear_wheel_cap.center.val[0],
+        rear_wheel_cap.center.val[1] - rear_wheel_cap.radius,
+        main_gear.center.val[0],
+        main_gear.center.val[1] - main_gear.radius);
+    line_draw(&top_chain, src, BLACK);
+    line_draw(&bottom_chain, src, BLACK);
+
+    Circle gear_base;
+    circle_set(
+        &gear_base,
+        main_gear.center,
+        2.5);
+    circle_drawFill(&gear_base, src, BLACK);
+
+    draw_spokes(rear_wheel, src, BLACK);
+    draw_spokes(front_wheel, src, BLACK);
 
     image_write(src, "output/lab3/illustration.ppm");
 

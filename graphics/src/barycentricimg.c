58b4e11f8c0e43f2eaf64f9f331690b89e186d49
Greg Attra <greg.attra@gmail.com>
2021-10-11 20:27:46 -0400

    baryblend

--- graphics/src/barycentricimg.c
+++ graphics/src/barycentricimg.c
@@ -9,15 +9,57 @@
 #include "graphics.h"
 #include <math.h>
 
+/**
+ * Draw the traingle given the points.
+ * 
+ * @param xa the x coord for point A
+ * @param ya the y coord for point A
+ * @param xb the x coord for point B
+ * @param yb the y coord for point B
+ * @param xc the x coord for point C
+ * @param yc the y coord for point C
+ * @param src the image to draw on
+ * 
+ * @return void
+ */
+static void draw_triangles(int xa, int ya, int xb, int yb, int xc, int yc, Image *src)
+{
+    Color ac =
+    {
+        (float) rand() / (float) RAND_MAX,
+        (float) rand() / (float) RAND_MAX,
+        (float) rand() / (float) RAND_MAX
+    };
+    Color bc =
+    {
+        (float) rand() / (float) RAND_MAX,
+        (float) rand() / (float) RAND_MAX,
+        (float) rand() / (float) RAND_MAX
+    };
+    Color cc =
+    {
+        (float) rand() / (float) RAND_MAX,
+        (float) rand() / (float) RAND_MAX,
+        (float) rand() / (float) RAND_MAX
+    };
+
+    Point vlist[3];
+    point_set2D(&vlist[0], xa, ya);
+    point_set2D(&vlist[1], xb, yb);
+    point_set2D(&vlist[2], xc, yc);
+    Polygon *t;
+    t = polygon_createp(3, vlist);
+    polygon_blendFillB(t, src, ac, bc, cc);
+    polygon_free(t);
+}
+
 int main(int argc, char *argv[])
 {
-    Color white = {1.0, 1.0, 1.0};
-
     int length = 900 / 5.5;
     int height = sqrt(((length/2)*(length/2) + (length * length)));
 
     Image *src;
-    src = image_create((height * 10) + 1, 900);
+    src = image_create((height * 5) + 1, 900);
     for (int i = height; i < src->rows; i+=height)
     {
         int y0 = i - height;
@@ -28,26 +70,21 @@ int main(int argc, char *argv[])
             int x0 = j - length;
             int x1 = j;
 
-            Point vlist[3];
-            point_set2D(&vlist[0], x0, y0);
-            point_set2D(&vlist[1], x1, y0);
-            point_set2D(&vlist[2], x0 + (length / 2), y1);
-            Polygon *t1;
-            t1 = polygon_createp(3, vlist);
-            
-            point_set2D(&vlist[0], x0 + (length / 2), y1);
-            point_set2D(&vlist[1], x1, y0);
-            point_set2D(&vlist[2], x1 + (length / 2), y1);
-            Polygon *t2;
-            t2 = polygon_createp(3, vlist);
-
-            polygon_draw(t1, src, white);
-            polygon_draw(t2, src, white);
-
-            polygon_free(t1);
-            polygon_free(t2);
+            draw_triangles(x0, y0, x1, y0, x0 + (length / 2), y1, src);
+            draw_triangles(x0 + (length / 2), y1, x1, y0, x1 + (length / 2), y1, src);
         }
     }
+
+    for (int i = height; i < src->rows; i+=height)
+    {
+        int y0 = i - height;
+        int y1 = i;
+
+        draw_triangles(0, y0, 0, y1, (length / 2), y1, src);
+        draw_triangles(src->cols - (length / 2), y0, src->cols, y0, src->cols, y1, src);
+    }
+
+
 
     image_write(src, "output/lab4/barycentric.ppm");
     image_free(src);

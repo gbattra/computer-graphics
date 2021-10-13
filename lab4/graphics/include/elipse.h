71134b3721f0b8236c775c2998bd953a5d9eb7d4
Greg Attra <greg.attra@gmail.com>
2021-10-13 10:45:51 -0400

    done w assignment 4

---
+++ lab4/graphics/include/elipse.h
@@ -0,0 +1,81 @@
+/**
+ * Greg Attra
+ * 10/01/2021
+ * 
+ * Defines the Elipse object and API.
+ */
+
+#ifndef ELIPSE
+#define ELIPSE
+
+#include "point.h"
+
+typedef struct
+{
+    double ra;
+    double rb;
+    double a;
+    Point c;
+} Elipse;
+
+/**
+ * Sets the provided attribute values on the elipse.
+ * 
+ * @param el the elipse to update
+ * @param ra the major axis radius
+ * @param rb the minor axis radius
+ * @param a the angle of the elipse relative to x axis
+ * 
+ * @return void
+ */
+void elipse_set(Elipse *el, Point c, double ra, double rb, double a);
+
+/**
+ * Draws the elipse on the image.
+ * 
+ * @param el the elipse to draw
+ * @param src the image to draw on
+ * @param c the color of the elipse
+ * 
+ * @return void
+ */
+void elipse_draw(Elipse *el, Image *src, Color c);
+
+/**
+ * Draw and fill an elipse.
+ * 
+ * @param el the elipse to draw
+ * @param src the image to draw on
+ * @param c the color of the elipse
+ * 
+ * @return void
+ */
+void elipse_drawFill(Elipse *el, Image *src, Color c);
+
+/**
+ * Draw the elipse with a gradient outline between two colors.
+ * 
+ * @param el the elipse to draw
+ * @param src the image to draw on
+ * @param ca the start color
+ * @param cb the end color
+ * @param vert vertical gradient flag
+ * 
+ * @return void
+ */
+void elipse_drawG(Elipse *el, Image *src, Color ca, Color cb, int vert);
+
+/**
+ * Draw the elipse with a gradient fill between two colors.
+ * 
+ * @param el the elipse to draw
+ * @param src the image to draw on
+ * @param ca the start color
+ * @param cb the end color
+ * @param vert vertical gradient flag
+ * 
+ * @return void
+ */
+void elipse_drawFillG(Elipse *el, Image *src, Color ca, Color cb, int vert);
+
+#endif
---
+++ lab4/graphics/include/graphics.h
@@ -0,0 +1,25 @@
+/**
+ * Greg Attra
+ * 10/08/2021
+ * 
+ * Wrapper header around all subheaders for the grpahics engine.
+ */
+
+#ifndef GRAPHICS
+#define GRAPHICS
+
+#include "image.h"
+#include "point.h"
+#include "line.h"
+#include "circle.h"
+#include "elipse.h"
+#include "polyline.h"
+#include "stack.h"
+#include "geometry.h"
+#include "list.h"
+#include "floodfill.h"
+#include "noise.h"
+#include "fractals.h"
+#include "polygon.h"
+
+#endif

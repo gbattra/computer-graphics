41d3ca1351efe90434e4ea9b3c32e4f5ccc55c37
Greg Attra <greg.attra@gmail.com>
2021-11-16 12:50:17 -0500

    fixed z buffer bug w lines

--- graphics/lib/line.c
+++ graphics/lib/line.c
@@ -25,18 +25,21 @@ void line_set2D(Line *l, double x0, double y0, double x1, double y1)
 {
     point_set2D(&l->a, x0, y0);
     point_set2D(&l->b, x1, y1);
+    l->zBuffer = 1;
 }
 
 void line_set3D(Line *l, double x0, double y0, double z0, double x1, double y1, double z1)
 {
     point_set3D(&l->a, x0, y0, z0);
     point_set3D(&l->b, x1, y1, z1);
+    l->zBuffer = 1;
 }
 
 void line_set(Line *l, Point ta, Point tb)
 {
     point_copy(&l->a, &ta);
     point_copy(&l->b, &tb);
+    l->zBuffer = 1;
 }
 
 void line_zBuffer(Line *l, int flag)
@@ -62,8 +65,8 @@ static void line_drawHorizontal(Image *src, Line *l, Color ca, Color cb)
     Color c;
     color_copy(&c, &ca);
 
-    int dx = fabs(l->b.val[0] - l->a.val[0]);
-    int x = l->a.val[0];
+    float dx = fabs(l->b.val[0] - l->a.val[0]);
+    float x = l->a.val[0];
     if (l->a.val[0] > l->b.val[0]) x = l->b.val[0];
     
     for (int i = 0; i < dx; i++)
@@ -80,8 +83,8 @@ static void line_drawVertical(Image *src, Line *l, Color ca, Color cb)
     Color c;
     color_copy(&c, &ca);
 
-    int dy = fabs(l->b.val[1] - l->a.val[1]);
-    int y = l->a.val[1];
+    float dy = fabs(l->b.val[1] - l->a.val[1]);
+    float y = l->a.val[1];
     if (l->a.val[1] > l->b.val[1]) y = l->b.val[1];
 
     for (int i = 0; i < dy; i++)
@@ -98,17 +101,30 @@ static void line_draw1(Image *src, Line *l, Color ca, Color cb)
     Color c;
     color_copy(&c, &ca);
 
-    int dy = l->b.val[1] - l->a.val[1];
-    int dx = l->b.val[0] - l->a.val[0];
-    int e = 2*dy - dx;
+    float dy = l->b.val[1] - l->a.val[1];
+    float dx = l->b.val[0] - l->a.val[0];
+    float dz = ((1.0/l->b.val[2]) - (1.0/l->a.val[2]))/dx;
+    float e = 2*dy - dx;
 
-    int y = l->a.val[1];
-    int x = l->a.val[0];
+    float y = l->a.val[1];
+    float x = l->a.val[0];
+    float z = 1.0/l->a.val[2];
+    printf("1: %f\n", z);
     for (int i = 0; i < abs(dx); i++)
     {
         double mag = ((double) i) / ((double) dx);
         color_interpolate(&c, &ca, &cb, mag);
-        image_setColor(src, y, x, c);
+        if ((l->zBuffer == 1 && z < image_getz(src, y, x)) || l->zBuffer == 0)
+        {
+            FPixel pix;
+            pix.a = 1.0;
+            pix.rgb[0] = c.c[0];
+            pix.rgb[1] = c.c[1];
+            pix.rgb[2] = c.c[2];
+            pix.z = z;
+            image_setf(src, y, x, pix);
+        }
+        z += dz;
 
         if (e > 0)
         {
@@ -125,18 +141,31 @@ static void line_draw2(Image *src, Line *l, Color ca, Color cb)
     Color c;
     color_copy(&c, &ca);
 
-    int dy = l->b.val[1] - l->a.val[1];
-    int dx = l->b.val[0] - l->a.val[0];
+    float dy = l->b.val[1] - l->a.val[1];
+    float dx = l->b.val[0] - l->a.val[0];
+    float dz = ((1.0/l->b.val[2]) - (1.0/l->a.val[2]))/dy;
 
-    int y = l->a.val[1];
-    int x = l->a.val[0];
+    float y = l->a.val[1];
+    float x = l->a.val[0];
+    float z = 1.0/l->a.val[2];
 
-    int e = 2*dx - dy;
+    printf("2: %f\n", z);
+    float e = 2*dx - dy;
     for (int i = 0; i < abs(dy); i++)
     {
         double mag = ((double) i) / ((double) dy);
         color_interpolate(&c, &ca, &cb, mag);
-        image_setColor(src, y, x, c);
+        if ((l->zBuffer == 1 && z < image_getz(src, y, x)) || l->zBuffer == 0)
+        {
+            FPixel pix;
+            pix.a = 1.0;
+            pix.rgb[0] = c.c[0];
+            pix.rgb[1] = c.c[1];
+            pix.rgb[2] = c.c[2];
+            pix.z = z;
+            image_setf(src, y, x, pix);
+        }
+        z += dz;
 
         if (e > 0)
         {
@@ -153,13 +182,16 @@ static void line_draw3(Image *src, Line *l, Color ca, Color cb)
     Color c;
     color_copy(&c, &ca);
 
-    int dy = l->b.val[1] - l->a.val[1];
-    int dx = l->b.val[0] - l->a.val[0];
+    float dy = l->b.val[1] - l->a.val[1];
+    float dx = l->b.val[0] - l->a.val[0];
+    float dz = ((1.0/l->b.val[2]) - (1.0/l->a.val[2]))/dy;
 
-    int y = l->b.val[1];
-    int x = l->b.val[0];
+    float y = l->b.val[1];
+    float x = l->b.val[0];
+    float z = 1.0/l->b.val[2];
+    printf("3: %f\n", z);
 
-    int e = 2*dx + dy;
+    float e = 2*dx + dy;
 
     for (int i = 0; i < abs(dy); i++)
     {
@@ -173,7 +205,17 @@ static void line_draw3(Image *src, Line *l, Color ca, Color cb)
         
         double mag = ((double) i) / ((double) dy);
         color_interpolate(&c, &ca, &cb, mag);
-        image_setColor(src, y, x, c);
+        if ((l->zBuffer == 1 && z < image_getz(src, y, x)) || l->zBuffer == 0)
+        {
+            FPixel pix;
+            pix.a = 1.0;
+            pix.rgb[0] = c.c[0];
+            pix.rgb[1] = c.c[1];
+            pix.rgb[2] = c.c[2];
+            pix.z = z;
+            image_setf(src, y, x, pix);
+        }
+        z += dz;
     }
     
 }
@@ -183,11 +225,15 @@ static void line_draw4(Image *src, Line *l, Color ca, Color cb)
     Color c;
     color_copy(&c, &ca);
 
-    int dy = l->b.val[1] - l->a.val[1];
-    int dx = l->b.val[0] - l->a.val[0];
+    float dy = l->b.val[1] - l->a.val[1];
+    float dx = l->b.val[0] - l->a.val[0];
+    float dz = ((1.0/l->b.val[2]) - (1.0/l->a.val[2]))/dx;
 
-    int y = l->b.val[1];
-    int x = l->b.val[0];
+    float y = l->b.val[1];
+    float x = l->b.val[0];
+    float z = 1.0/l->b.val[2];
+
+    printf("4: %f\n", z);
 
     int e = 2*dy + dx;
     for (int i = 0; i < abs(dx); i++)
@@ -202,7 +248,17 @@ static void line_draw4(Image *src, Line *l, Color ca, Color cb)
 
         double mag = ((double) i) / ((double) dx);
         color_interpolate(&c, &ca, &cb, mag);
-        image_setColor(src, y, x, c);
+        if ((l->zBuffer == 1 && z < image_getz(src, y, x)) || l->zBuffer == 0)
+        {
+            FPixel pix;
+            pix.a = 1.0;
+            pix.rgb[0] = c.c[0];
+            pix.rgb[1] = c.c[1];
+            pix.rgb[2] = c.c[2];
+            pix.z = z;
+            image_setf(src, y, x, pix);
+        }
+        z += dz;
     }
 }
 
@@ -213,32 +269,38 @@ void line_draw(Line *l, Image *src, Color c)
 
 void line_drawG(Line *l, Image *src, Color ca, Color cb)
 {
-    int y0 = l->a.val[1];
-    int y1 = l->b.val[1];
-    int x0 = l->a.val[0];
-    int x1 = l->b.val[0];
+    float x0 = l->a.val[0];
+    float y0 = l->a.val[1];
+    float z0 = l->a.val[2];
+
+    float x1 = l->b.val[0];
+    float y1 = l->b.val[1];
+    float z1 = l->b.val[2];
 
     if (x0 > x1)
     {
-        int xt = x0;
+        float xt = x0;
         x0 = x1;
         x1 = xt;
 
-        int yt = y0;
+        float yt = y0;
         y0 = y1;
         y1 = yt;
+
+        float zt = z0;
+        z0 = z1;
+        z1 = zt;
     }
 
     Line tmp;
-    tmp.zBuffer = l->zBuffer;
-    line_set3D(&tmp, x0, y0, l->a.val[2], x1, y1, l->b.val[2]);
+    line_set3D(&tmp, x0, y0, z0, x1, y1, z1);
 
     if (y0 == y1) return line_drawHorizontal(src, &tmp, ca, cb);
     if (x0 == x1) return line_drawVertical(src, &tmp, ca, cb);
 
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    float m = (float) dy / (float) dx;
+    float dx = x1 - x0;
+    float dy = y1 - y0;
+    float m = dy / dx;
 
     if (0 < m && m <= 1) return line_draw1(src, &tmp, ca, cb);
     if (m > 1) return line_draw2(src, &tmp, ca, cb);

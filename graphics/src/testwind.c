a935f47e990879359270e22a9a626b81d4673d77
Greg Attra <greg.attra@gmail.com>
2021-12-08 16:35:38 -0500

    added test wind

--- graphics/src/testwind.c
+++ graphics/src/testwind.c
@@ -2,7 +2,7 @@
  * Greg Attra
  * 12/08/2021
  * 
- * Test the friction force on a rigidbody.
+ * Test the wind force on a rigidbody.
  */
 
 #include "graphics.h"
@@ -13,10 +13,6 @@
 
 int main(int argc, char *argv[])
 {
-    float friction = atof(argv[1]);
-    if( friction < 0 || friction > 1 )
-        return 1;
-
     int rows, cols, nFrames;
     Matrix VTM, GTM;
     Color White, Grey,DkGrey;
@@ -26,14 +22,14 @@ int main(int argc, char *argv[])
 
     rows = 900;
     cols = 900;
-    nFrames = 40;
+    nFrames = 25;
 
     color_set( &White, 1.0, 1.0, 1.0 );
     color_set( &Grey, 0.6, 0.62, 0.64 );
     color_set( &DkGrey, 0.1, 0.1, 0.1 );
 
-	float d = 25;
-	point_set3D(&view.vrp, d/3.5, 0, d);
+	float d = 30;
+	point_set3D(&view.vrp, d/3.5, -d/3.5, d);
 	vector_set( &(view.vpn), 0, 0, -d);
 	vector_set( &(view.vup), 0.0, 1.0, 0.0 );
 	view.d = 1.5;
@@ -61,13 +57,11 @@ int main(int argc, char *argv[])
 
     Rigidbody *rb = rigidbody_create();
     rigidbody_setMass(rb, 1);
-    rigidbody_setUseGravity(rb, 0);
-    rigidbody_setFriction(rb, friction);
 
-    Vector fdir;
-    vector_set(&fdir, 1, 0, 0);
-    Force *f = force_createp(&fdir, 30);
-    rigidbody_addForce(rb, f);
+    Vector windDir;
+    vector_set(&windDir, 1, 0, 0);
+    Force *wind = force_createp(&windDir, 60);
+    rigidbody_addForce(rb, wind);
 
     for (int i = 0; i < nFrames; i++)
     {
@@ -80,8 +74,8 @@ int main(int argc, char *argv[])
         module_bodyColor(sphere, White);
         module_surfaceColor(sphere, DkGrey);
         module_sphere(sphere, 4, 1);
+        
         rigidbody_setModule(rb, sphere);
-
         rigidbody_tick(rb);
         rigidbody_render(rb, md);
 
@@ -89,10 +83,7 @@ int main(int argc, char *argv[])
         module_delete(md);
 
         char buffer[256];
-        if (friction > 0)
-            sprintf(buffer, "final/testfrictionon/frictionon_frame%03d.ppm", i);
-        else 
-            sprintf(buffer, "final/testfrictionoff/frictionoff_frame%03d.ppm", i);
+        sprintf(buffer, "final/testwind/testwind_frame%03d.ppm", i);
         image_write(src, buffer);
 
         image_reset(src);
